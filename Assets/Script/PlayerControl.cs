80268ec161472b59a17f8f06f4bd351d2383fe62
2172695 <2172695@TE-GDA-D-D37H13>
2024-02-14 12:34:15 +0100

    Dodaj pliki projektów.

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        
+
+        Vector3 movement = Vector3.right * x * Time.deltaTime;
+
+        float y = Input.GetAxis("Vertical");
+
+        movement += Vector3.forward * y * Time.deltaTime;
+
+        movement = movement.normalized;
+
+        movement *= moveSpeed;
+
+        transform.position += movement;
+
+
+    }
+}
